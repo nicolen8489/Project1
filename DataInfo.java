8db5d8db7020fc23246ef35d65eb3e66ff3abade
Nicole Nelson <nicole.nelson8489@gmail.com>
2012-02-16 20:13:11 -0500

    initial import

---
+++ DataInfo.java
@@ -0,0 +1,9 @@
+public class DataInfo {
+  private double maxSatClausePercent;
+  public DataInfo() {
+    maxSatClausePercent = 0.0; 
+  }
+  public double getMaxSatClausePercent() {
+    return maxSatClausePercent;
+  }
+}
